6edd10cc6fde0d7c5798e1d3f2f7c219638d6ebd
Gabriel Mendes Rangel <gabrielmendesrangel2@gmail.com>
2023-04-18 22:28:06 -0300

    Aula 03

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 const form = document.getElementById('novoItem');
 const lista = document.getElementById('lista');
+const itens = JSON.parse(localStorage.getItem("itens")) || [];
+
+itens.forEach( (elemento) => {
+    criaElemento(elemento);
+});
 
 form.addEventListener('submit', (evento) => {
     evento.preventDefault();
@@ -7,18 +12,29 @@ form.addEventListener('submit', (evento) => {
     const nome = evento.target.elements['nome'].value;
     const quantidade = evento.target.elements['quantidade'].value;
 
-    criaElemento(nome, quantidade);
+    const itemAtual = {
+        "nome": nome,
+        "quantidade": quantidade
+    }
+
+    criaElemento(itemAtual);
+
+    itens.push(itemAtual);
+
+    localStorage.setItem("itens", JSON.stringify(itens));
+
+    form.reset();
 });
 
-function criaElemento(nome, quantidade){
+function criaElemento(itemAtual){
     const novoItem = document.createElement('li');
     novoItem.classList.add("item");
 
     const numeroItem = document.createElement("strong");
-    numeroItem.innerHTML = quantidade;
+    numeroItem.innerHTML = itemAtual.quantidade;
 
     novoItem.appendChild(numeroItem);
-    novoItem.innerHTML += nome;
+    novoItem.innerHTML += itemAtual.nome;
 
     lista.appendChild(novoItem);
 }
